0ea6105e521becfe40a7dbbc3ee8e79e8716a8cb
TrixzyMc <trixzymc@gmail.com>
2026-06-29 18:31:31 -0400

    bStats Integration

--- src/main/java/git/otrixzy/ocooldowns/OCooldowns.java
+++ src/main/java/git/otrixzy/ocooldowns/OCooldowns.java
@@ -1,17 +1,23 @@
 package git.otrixzy.ocooldowns;
 
+import com.google.j2objc.annotations.Property;
 import git.otrixzy.ocooldowns.api.OCooldownsAPI;
 import git.otrixzy.ocooldowns.internal.CommandListener;
 import git.otrixzy.ocooldowns.internal.CooldownManager;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bstats.bukkit.Metrics;
+import org.bstats.charts.SimplePie;
+import org.bstats.charts.SingleLineChart;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OCooldowns extends JavaPlugin {
-
     private CooldownManager cooldownManager;
 
     @Override
     public void onEnable() {
+        int pluginId = 32289;
+        Metrics metrics = new Metrics(this, pluginId);
+
         saveDefaultConfig();
 
         this.cooldownManager = new CooldownManager();

f30f5f818982154142480abc1830ea069931678c
TrixzyMc <trixzymc@gmail.com>
2026-06-29 00:37:21 -0400

    Startup

---
+++ src/main/java/git/otrixzy/ocooldowns/OCooldowns.java
@@ -0,0 +1,17 @@
+package git.otrixzy.ocooldowns;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OCooldowns extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
